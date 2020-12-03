115266953f05bfe0d16bb6abbfdcab39be87686a
RESHMA4887 <reshmavchandranpranavam@gmail.com>
2020-12-03 19:13:13 +0530

    day9

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LINK UR CODES</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="main.html">Link</a>
+          </li>
+         
+        </ul>
+      </nav>
+    <div class="bck">
+        <center><p class="textstyle">LINK UR CODES</p></center>
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-xl-4"></div>
+            <div class="col col-12 col-sm-4 col-xl-4">
+                <table class="table">
+                    <tr>
+                        <td>Registration Id</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Pasaword</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Student</td>
+                        <td><input type="checkbox" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Working</td>
+                        <td><input type="checkbox" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn-info">OK</button></td>
+                    </tr>
+                </table>
+                
+            </div>
+            
+            <div class="col col-12 col-sm-4 col-xl-4"></div>
+        
+         </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="main.html">Link</a>
+          </li>
+         
+        </ul>
+      </nav>
+    <div class="conatainer">
+        <center><p class="textstyle">contact</p></center>
+        <div class="row">
+          <div class="col col-12 col-sm-4 col-xl-4"></div>
+          <div class="col col-12 col-sm-4 col-xl-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia expedita excepturi cum tempora ut vitae, porro, possimus numquam voluptas quae optio. Facilis, nihil. Provident distinctio incidunt quo, repudiandae omnis vero?
+       </div>
+          <div class="col col-12 col-sm-4 col-xl-4"></div>
+         </div>
+    </div>
+    
+</body>
+</html>
